1082b61609148252cbeeabc05229e4c64d92c433
vibhanimbalkar97-star <vibhanimbalkar97@gmail.com>
2025-11-12 17:10:01 +0530

    background UI done
    .

--- chatgpt_ai_tool/src/App.jsx
+++ chatgpt_ai_tool/src/App.jsx
@@ -5,7 +5,19 @@ function App() {
 
   return (
     <>
-      
+      <div className='h-screen grid grid-cols-5 text-center'>
+        <div className='col-span-1 bg-zinc-800'></div>
+        <div className='col-span-4 bg-zinc-900 p-10'>
+          <div className='containe h-110'>
+          </div>
+          <div className='bg-zinc-800 w-1/2 p-1 pr-5 text-white rounded-4xl m-auto flex border border-zinc-700 h-16'>
+            <input type='text' className='p-3 w-full h-full outline-none' placeholder='Ask me anything' />
+            <button>Ask</button>
+          </div>
+
+        </div>
+
+      </div>
     </>
   )
 }
